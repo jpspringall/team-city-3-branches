4db81c3656524903b92db1e0f850d919387e473a
James Springall <james_springall@hotmail.com>
2024-09-13 18:43:57 +1000

    yarp

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -40,124 +40,12 @@ object MasterBuild : BuildType({
     name = buildTypeName
     id = RelativeId(buildTypeName.toId())
 
-    vcs {
-        root(DslContext.settingsRoot)
-        cleanCheckout = true
-        excludeDefaultBranchChanges = true
-    }
-
-    params {
-        param("git.branch.specification", "")
-    }
-
-    createParameters()
-
-    printPullRequestNumber()
-
-    runMakeTest()
-
-    buildAndTest()
-
-    runSonarScript()
-
-    triggers {
-        vcs {
-        }
-    }
-
-    features {}
-})
-
-object PullRequestBuild : BuildType({
-    val buildTypeName = "Pull Request Build"
-    name = buildTypeName
-    id = RelativeId(buildTypeName.toId())
-
-    vcs {
-        root(DslContext.settingsRoot)
-        cleanCheckout = true
-        excludeDefaultBranchChanges = true
-    }
-
-    params {
-        param("git.branch.specification", "+:refs/pull/*/merge")
-    }
-    createParameters()
-
-    printPullRequestNumber()
-
-    runMakeTest()
-
-    buildAndTest()
-
-    runSonarScript()
-
-    triggers {
-        vcs {
-        }
-    }
-
-    features {
-//        commitStatusPublisher {
-//            vcsRootExtId = DslContext.settingsRootId.value
-//            publisher = github {
-//                githubUrl = "https://api.github.com"
-//                authType = vcsRoot()
-//            }
-//        }
-//        pullRequests {
-//            vcsRootExtId = DslContext.settingsRootId.value
-//            provider = github {
-//                authType = vcsRoot()
-//                filterSourceBranch = "refs/pull/*/merge"
-//                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
-//            }
-//        }
-    }
-})
-
-object DeployBuild : BuildType({
-    val buildTypeName = "Deploy Build"
-    name = buildTypeName
-    id = RelativeId(buildTypeName.toId())
-
-
-    vcs {
-        root(DslContext.settingsRoot)
-        cleanCheckout = true
-        excludeDefaultBranchChanges = true
-    }
-
-    buildNumberPattern = MasterBuild.depParamRefs.buildNumber.toString()
-
-    dependencies {
-        snapshot(MasterBuild) {
-            onDependencyFailure = FailureAction.FAIL_TO_START
-            onDependencyCancel = FailureAction.CANCEL
-        }
-    }
-
-    params {
-        param("git.branch.specification", "")
-    }
-
-    createParameters()
-
-    printDeployNumber()
-
-    triggers {
-        vcs {
-        }
-    }
-
-    features {}
 })
 
 val builds: ArrayList<BuildType> = arrayListOf()
 
 builds.add(MasterBuild)
-builds.add(PullRequestBuild)
-builds.add(DeployBuild)
+
 
 val project = Project {
     vcsRoot(DslContext.settingsRoot)
@@ -168,34 +56,5 @@ val project = Project {
 
     buildTypesOrder = builds
 }
-
-//for (bt : BuildType in project.buildTypes ) {
-//    bt.paused = false
-//    val gitSpec = bt.params.findRawParam("git.branch.specification")
-//    if (gitSpec != null && gitSpec.value.isNotBlank()) {
-//        bt.vcs.branchFilter = """
-//            +:*
-//            -:<default>
-//        """.trimIndent()
-//    }
-//    if (bt.name == "Pull Request Build" || bt.name == "Master Build") {
-//        bt.features.add {
-//            feature {
-//                type = "xml-report-plugin"
-//                param("verbose", "true")
-//                param("xmlReportParsing.reportType", "trx")
-//                param("xmlReportParsing.reportDirs","%system.teamcity.build.checkoutDir%/test-results/**/*.trx")
-//            }
-//        }
-//    }
-//    if (bt.name == "Pull Request Build" || bt.name == "Master Build")
-//    {
-//        bt.features.add {  xmlReport {
-//            reportType = XmlReport.XmlReportType.TRX
-//            rules = "%system.teamcity.build.checkoutDir%/test-results/**/*.trx" //Remember to match this in test output
-//            verbose = true
-//        } }
-//    }
-//}
 
 project(project)
